ea0142a1ef153cd43cdf0b217358f321df71689e
Stilyan Dimitrov <sti4o_21@aol.com>
2024-04-23 00:47:34 -0500

    added the pages and the same css styling

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('products/', views.index, name='index'),
+    path('products/<int:product_id>/', views.show, name='show'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Product
+
+# Index view to list all products
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'myapp/index.html', {'products': products})
+
+# Show view to display a specific product by ID
+def show(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'myapp/show.html', {'product': product})
